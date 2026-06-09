13944fa8b310754263c7830153671bc56eb13d0b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2026-06-09 18:51:38 +0600

    feat(header): implement header component and import required assests

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import logo from "../assets/logo.svg";
+import ring from "../assets/ring.svg";
+import moon from "../assets/icons/moon.svg";
+import shoppingCart from "../assets/shopping-cart.svg";
+
 export default function Header() {
   return (
     <header>
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
-          <img src="./assets/logo.svg" width="139" height="26" alt="" />
+          <img src={logo} width="139" height="26" alt="" />
         </a>
 
         <ul className="flex items-center space-x-5">
@@ -12,7 +17,7 @@ export default function Header() {
               className="bg-primary/20 dark:bg-primary/7 rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img src="./assets/ring.svg" width="24" height="24" alt="" />
+              <img src={ring} width="24" height="24" alt="" />
             </a>
           </li>
           <li>
@@ -21,7 +26,7 @@ export default function Header() {
               href="#"
             >
               <img
-                src="./assets/icons/moon.svg"
+                src={moon}
                 width="24"
                 height="24"
                 alt=""
@@ -34,7 +39,7 @@ export default function Header() {
               href="#"
             >
               <img
-                src="./assets/shopping-cart.svg"
+                src={shoppingCart}
                 width="24"
                 height="24"
                 alt=""
